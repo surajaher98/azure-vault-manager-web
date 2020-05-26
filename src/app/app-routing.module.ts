2614d5ad0bda5431fb5645f255466bf4740fb136
Suraj Aher <suraj.aher@nitorinfotech.com>
2020-05-26 09:36:29 +0530

    project created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MsalGuard } from '@azure/msal-angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' , canActivate : [MsalGuard] },
+    { path: 'dashboard', component: AppComponent, canActivate : [MsalGuard] }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
