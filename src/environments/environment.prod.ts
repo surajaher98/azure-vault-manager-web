4403969582dfaeeed492e3bb73af74b30b046290
surajaher98 <45867140+surajaher98@users.noreply.github.com>
2020-06-08 19:07:15 +0530

    Update environment.prod.ts
    remove the secrets from .prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  clientID: 'd93de0aa-af9b-47f2-b1b6-5bff81d61001',
-  authority: 'https://login.microsoftonline.com/8c3dad1d-b6bc-4f8b-939b-8263372eced6',
-  redirectUri: 'https://azurekeyvaultweb.z19.web.core.windows.net/'
+  clientID: '',
+  authority: '',
+  redirectUri: ''
 };
